116470111df5e1b9fb4b4954eb1e8da4071589fd
Kristoffer Strekerud <kristofferstrekerud@gmail.com>
2012-12-05 22:15:51 +0100

    Ball + Paddle
    Lagt til Ball- og Paddle-klassene m/ konstruktører (foreløpige)

---
+++ src/Ball.java
@@ -0,0 +1,11 @@
+
+public class Ball {
+
+	double x_pos;
+	double y_pos;
+	
+	Ball(double x_pos, double y_pos) {
+		this.x_pos = x_pos;
+		this.y_pos = y_pos;
+	}
+}
---
+++ src/Paddle.java
@@ -0,0 +1,16 @@
+
+public class Paddle {
+	
+	int width;
+	int height;
+	
+	double x_pos;
+	double y_pos;
+	
+	Paddle(int width, int height, double x_pos, double y_pos) {
+		this.width = width;
+		this.height = height;
+		this.x_pos = x_pos;
+		this.y_pos = y_pos;
+	}
+}
