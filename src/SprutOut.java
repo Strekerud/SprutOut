7fcf68e30a8fe96e35fb78409216719b76c2577f
Kristoffer Strekerud <kristofferstrekerud@gmail.com>
2012-12-05 20:31:02 +0100

    Info ang gameloop

--- src/SprutOut.java
+++ src/SprutOut.java
@@ -16,19 +16,19 @@ public class SprutOut extends BasicGame {
 
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
+		//All tegning av objekter
 		
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
+		//Her initialiseres alt
 		
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
-		// TODO Auto-generated method stub
+		//Alle oppdatering (game logic)
 		
 	}
 	
