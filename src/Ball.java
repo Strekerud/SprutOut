02d5cfcf15694beac4267071b6b71adf71d8cf1c
Emil Strøm <emil.strom90@gmail.com>
2012-12-08 01:52:45 +0100

    Lagt inn borders for ball og en pause funksjon
    Har lagt inn borderene til brette til ball og lagt til "P" som pause,
    har ikke ennå fikset kollisjon mot paddle. NN

--- src/Ball.java
+++ src/Ball.java
@@ -1,11 +1,46 @@
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Circle;
+
+
 public class Ball {
 
+	double radius;
+	
 	double x_pos;
 	double y_pos;
 	
-	Ball(double x_pos, double y_pos) {
+	Image ballImage;
+	Circle ballShape;
+	
+	double ballSpeed = 4.0;
+	
+	Ball(double radius,double x_pos,double y_pos) {
 		this.x_pos = x_pos;
 		this.y_pos = y_pos;
+		this.radius = radius;
+		ballShape = new Circle((float)x_pos,(float)y_pos,(float)radius);
 	}
+	
+	public void setBallImage(Image ballImage){
+		this.ballImage = ballImage;
+	}
+	
+	public Image getBallImage(){
+		return ballImage;
+	}
+	
+	public Circle getBallShape(){
+		return ballShape;
+	}
+	
+	public void setBallSpeed(int speed){
+		this.ballSpeed = speed;
+	}
+	
+	
+	
 }
+
+
+
--- src/SprutOut.java
+++ src/SprutOut.java
@@ -24,10 +24,16 @@ public class SprutOut extends BasicGame {
 	Image menuClick;
 	static private Music openMenuMusic;
 	boolean menuTime = true;
+	boolean paused = false;
+	
+	Image pause;
+	
 	
 	Paddle p; 
+	Ball b;
 	
 	Image borderImage;
+	char dir;
 	
 		
 	public SprutOut() {
@@ -47,6 +53,7 @@ public class SprutOut extends BasicGame {
 		else {
 			//Tegner paddle (må erstattes av bilde)
 			arg1.drawRect((float)p.x_pos,(float) p.y_pos,(float) p.width,(float) p.height);
+			arg1.drawOval((float)b.x_pos,(float)b.y_pos, (float)b.radius, (float)b.radius);
 			//Tegner borders (også midlertidig)
 			arg1.drawRect(0,0,100,screenHeight-1);
 			arg1.drawRect(screenWidth - 100,0,100,screenHeight-1);
@@ -69,33 +76,88 @@ public class SprutOut extends BasicGame {
 		
 		//Oppretter paddelen
 		p = new Paddle(100,25,200,screenHeight - 26);
+		//Oppretter ballen
+		b = new Ball(25,150,150);
+		dir = 's';
+		
 		
 		borderImage = new Image("res/img/border_placeholder.png");
 		
 	
 	}
-	public boolean border(double x_pos,double y_pos){
-		if(x_pos > 0 && x_pos < screenWidth){
-			return true;
-		}else{
-			return false;
+	public void moveBall(double cur_x_pos,double cur_y_pos,char direction){
+		if(direction == 'n'){ // opp til høyre
+			b.x_pos = b.x_pos + b.ballSpeed;
+			b.y_pos = b.y_pos - b.ballSpeed;
+		}else if(direction == 's'){ //ned til venstre
+			b.x_pos = b.x_pos - b.ballSpeed;
+			b.y_pos = b.y_pos + b.ballSpeed;
+		}else if(direction == 'e'){ // opp til venstre
+			b.x_pos = b.x_pos - b.ballSpeed;
+			b.y_pos = b.y_pos - b.ballSpeed;
+		}else if(direction == 'w'){ // ned til høyre
+			b.x_pos = b.x_pos + b.ballSpeed;
+			b.y_pos = b.y_pos + b.ballSpeed;
 		}
 	}
+	
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		//Alle oppdatering (game logic)
 		Input input = arg0.getInput();
+		if(menuTime == false&&paused == false){
+			//Dette er den peneste testen ever:D
+			if(b.x_pos-b.ballSpeed>leftBorder&&b.x_pos+b.ballSpeed+b.radius-100<rightBorder&&b.y_pos+b.ballSpeed>0.0&&b.y_pos+b.ballSpeed+b.radius<screenHeight){
+				moveBall(b.x_pos,b.y_pos,dir);
+			}else if(b.x_pos-b.ballSpeed<=leftBorder){
+				if(dir == 'e'){
+					dir = 'n';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}else if(dir == 's'){
+					dir = 'w';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}
+			}else if(b.x_pos+b.ballSpeed+b.radius-100>=rightBorder){
+				if(dir == 'n'){
+					dir = 'e';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}else if(dir == 'w'){
+					dir = 's';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}
+			}else if(b.y_pos+b.ballSpeed<=0.0){
+				if(dir == 'n'){
+					dir = 'w';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}else if(dir == 'e'){
+					dir = 's';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}
+			}else if(b.y_pos+b.ballSpeed+b.radius >= screenHeight){
+				if(dir == 's'){
+					dir = 'e';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}else if(dir == 'w'){
+					dir = 'n';
+					moveBall(b.x_pos,b.y_pos,dir);
+				}
+			}
+		}		
 		
-		
-		 
 	    if(menuTime && input.isKeyDown(Input.KEY_SPACE)) {
 	    	menuTime = false;
 	    	menuLogo.destroy();
 	    	menuClick.destroy();
+	    		    			 
 	    }
-	    
-	    	    
-	    else if(input.isKeyDown(Input.KEY_RIGHT)) {
+	    else if(input.isKeyPressed(Input.KEY_P)){
+	    	if(paused == false){
+	    		paused = true;
+	    	}else if (paused == true){
+	    		paused = false;
+	    	}
+	    }
+	    else if(input.isKeyDown(Input.KEY_RIGHT)&&paused == false) {
 	    	if(p.x_pos == rightBorder){
 	    		p.x_pos += 0.0;
 	    	}else{
@@ -103,14 +165,14 @@ public class SprutOut extends BasicGame {
 	    	}
 	    }
 	    
-	    else if(input.isKeyDown(Input.KEY_LEFT)) {
+	    else if(input.isKeyDown(Input.KEY_LEFT)&&paused == false) {
 	    	if(p.x_pos == leftBorder){
 	    		p.x_pos += 0.0;
-	    	}else{
+	    	}else{	
 	    		p.x_pos -= p.paddleSpeed;
 	    	}
-	    }
-		
+	    }	    
+	    		
 	}
 	
 	public static void main(String[] args) throws SlickException {
